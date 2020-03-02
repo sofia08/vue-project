f93ea3632181345b1bf6d880c694d8c25522b5a1
Sofia Kalra <sofia@workingmouse.com.au>
2020-03-02 13:30:06 +1000

    Add fetch and filter races with some styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app' class='container'>
+    <div class='row my-2'>
+      <h1 class="col-10">Next to Go</h1>
+      <select class='dropdfown-menu col-2' v-model='filter' v-on:change="() => this.fetchRaces()">
+        <option class='dropdown-item' value='4a2788f8-e825-4d36-9894-efd4baf1cfae'>Horse</option>
+        <option class='dropdown-item' value='161d9be2-e909-4326-8c2c-35ed71fb460b'>Harness</option>
+        <option class='dropdown-item' value='9daef0d7-bf3c-4f50-921d-8e818c60fe61'>Greyhound</option>
+      </select>
+    </div>
+    <div class='row'>
+    <Races v-model='races' v-on:change='this.updateRaces'/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Races from './components/Races.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Races
+  },
+  created() {
+    // Once the component has been mounted, fetch the races
+    this.fetchRaces()
+  },
+  data() {
+    return {
+      filter: '4a2788f8-e825-4d36-9894-efd4baf1cfae', // Default the filter to horses 
+      races: []
+    }
+  },
+  methods: {
+    updateRaces() {
+
+    },
+    fetchRaces(count) {
+      // Default the count to 10 if it is undefined
+      if (count === undefined) {
+        count = 30
+      }
+
+      // Fetch the data using the API
+      fetch(`https://api.neds.com.au/rest/v1/racing/?method=nextraces&count=${count}`)
+        .then((response) => response.json())
+        .then((response) => {
+          const raceList = response.data.race_summaries
+          const ids = response.data.next_to_go_ids
+
+          // Fetch more races if we don't get at least 5 races for the filter type
+          if (this.filterRaces(ids, raceList) !== 5 && count <= 200) {
+            this.fetchRaces(count + 10)
+          }
+        })
+        .catch((e) => console.error('Could not fetch races', e))
+    },
+
+    filterRaces(keys, raceList) {
+      this.races = []
+      
+      for (const key of keys) {
+        let race = raceList[key]
+
+        // Stop if we have found 5 races to display
+        if (this.races.length == 5) {
+          break;
+        }
+
+        if (race.category_id == this.filter){
+          this.races.push(race)
+        }
+      }
+
+      return this.races.length;
+    }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+select {
+  height: 50px;
+  vertical-align: middle;
 }
 </style>
---
+++ src/components/Races.vue
@@ -0,0 +1,65 @@
+<template>
+    <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Meeting Name</th>
+                <th scope="col">Race Number</th>
+                <th scope="col">Countdown </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-if='value.length == 0'>
+                There are no items to display
+            </tr>
+            <tr v-else v-for='item in value' :key='item.race_id'>
+                <td>{{ item.meeting_name }}</td>
+                <td>{{ item.race_number }}</td>
+                <td>{{ formatCountdown(item.advertised_start.seconds) }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'Races',
+    props: {
+        value: Array
+    },
+    data () {
+        return {
+            time: this.getCurrentTime()
+        }
+    },
+    created() {
+        // Update the timer every second
+        window.setInterval(() => this.time = this.getCurrentTime(), 1000)
+    },
+    methods: {
+        getCurrentTime() {
+            return Math.floor(Date.now() / 1000)
+        },
+        formatCountdown(start_time) {
+            let time_left = start_time - this.time
+
+            // The event is occurring
+            if (time_left <= 0) {
+                if (time_left <= -60) {
+                    console.log("Remove element from the list & update it")
+                    // TODO: Need to remove once it has started
+                }
+                return 'now'
+            } else if (time_left / 60 >= 1) {
+                return `${Math.ceil(time_left / 60)}m`
+            }
+
+            return `${start_time - this.time}s`
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+
+</style>
